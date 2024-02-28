8aa1f0dada9cfe4ebb941f6f1fbe661468a7e164
Ayush Shukla <123shuklaayush@gmail.com>
2024-02-28 11:47:28 +0530

    added few pages

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,32 +1,22 @@
-import Image from "next/image";
-import Link from "next/link";
+import React from "react";
 import styles from "./menu.module.css";
+import Link from "next/link";
+import Image from "next/image";
+import MenuPosts from "../menuPosts/MenuPosts";
+import MenuCategories from "../menuCategories/MenuCategories";
 
 const Menu = () => {
   return (
     <div className={styles.container}>
       <h2 className={styles.subtitle}>{"What's hot"}</h2>
       <h1 className={styles.title}>Most Popular</h1>
-      <div className={styles.items}>
-        <Link href="/" className={styles.item}>
-          <div className={styles.imageContainer}>
-            <Image src="/p1.jpeg" alt="" fill className={styles.image} />
-          </div>
-          <div className={styles.textContainer}>
-            <span className={`${styles.category} ${styles.travel}`}>
-              Travel
-            </span>
-            <h3 className={styles.postTitle}>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Obcaecati, aspernatur
-            </h3>
-            <div className={styles.detail}>
-              <span className={styles.username}>John Doe</span>
-              <span className={styles.date}>26.02.2023</span>
-            </div>
-          </div>
-        </Link>
-      </div>
+      <MenuPosts withImage={false} />
+      <h2 className={styles.subtitle}>Discover by topic</h2>
+      <h1 className={styles.title}>Categories</h1>
+      <MenuCategories />
+      <h2 className={styles.subtitle}>Chosen by the editor</h2>
+      <h1 className={styles.title}>Editors Pick</h1>
+      <MenuPosts withImage={true} />
     </div>
   );
 };
