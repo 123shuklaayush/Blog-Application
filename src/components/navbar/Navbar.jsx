c10d33b2b4dd009ceeb193b92192b0ea773d4c25
Ayush Shukla <123shuklaayush@gmail.com>
2024-02-29 23:31:30 +0530

    fix bugs

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "./navbar.module.css";
 import Image from "next/image";
 import Link from "next/link";
-import AuthLinks from "../authLinks/AuthLinks";
+import AuthLinks from "../authlinks/AuthLinks";
 import ThemeToggle from "../themeToggle/ThemeToggle";
 
 const Navbar = () => {
